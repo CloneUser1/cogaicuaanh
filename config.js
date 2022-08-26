c2395256fcf26526f7669326aa85e6c5d0871962
CloneUser1 <112144821+CloneUser1@users.noreply.github.com>
2022-08-26 10:07:04 +0700

    Update
    Main

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '30-08-2000';
+const giftUrl = 'https://drive.google.com/drive/folders/1IAskHzFffutq5eWlc6BVmypsIkcrN-l0?usp=sharing';
+const eventName = 'Gửi tặng em';
+const titleCard = 'CÔ GÁI HỌ NGUYỄN';
+const contentCard = '❤ Chúc em, một nửa của đời anh luôn cảm thấy hạnh phúc khi có anh luôn bên cạnh. Dù em có đi tới đâu anh vẫn theo từng bước chân em. Anh chỉ mong được theo em trên đoạn đường còn lại để có thể dìu em khi em ngã. Hạnh phúc khi được thấy em cười và bên cạnh em khi cô đơn nhất, với anh như thế là đủ. Mãi yêu em. Chúc em sinh nhật vui vẻ ❤';
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+
